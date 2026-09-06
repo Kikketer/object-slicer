f2074a2fd491404e34290d0504e79813a4c0fba1
Chris Weed <chris@cjweed.com>
2026-09-06 09:00:42 -0500

    I removed the .ts extension from the engine import so the language server stops complaining.

--- src/bun/index.ts
+++ src/bun/index.ts
@@ -9,7 +9,7 @@ import {
 import * as fs from "fs";
 import * as os from "os";
 import * as path from "path";
-import { sliceStl } from "../../engine/slicer.ts";
+import { sliceStl } from "../../engine/slicer";
 
 type SliceInputs = {
   stlPath: string;
